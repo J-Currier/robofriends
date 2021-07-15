616fad54e4cdd3b249bbc9a66b797431180b6abd
unknown <j.currier224@gmail.com>
2021-07-15 14:00:51 -0600

    robofriends iniital commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from './Hello';
+import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Hello greeting={'Hello out there'}/>
+    {/* <App /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
